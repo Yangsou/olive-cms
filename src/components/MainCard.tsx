9e7cf84b25e8e274f012fa5636deed52937b9f98
yangsou <namnguyentrung95@gmail.com>
2023-02-17 17:30:34 +0700

    first commint

---
+++ src/components/MainCard.tsx
@@ -0,0 +1,107 @@
+import PropTypes from 'prop-types';
+import { forwardRef, ReactNode } from 'react';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Card, CardContent, CardHeader, Divider, Typography } from '@mui/material';
+
+// project import
+import Highlighter from './third-party/Highlighter';
+
+// header style
+const headerSX = {
+    p: 2.5,
+    '& .MuiCardHeader-action': { m: '0px auto', alignSelf: 'center' }
+};
+
+// ==============================|| CUSTOM - MAIN CARD ||============================== //
+type Props = {
+  border: boolean;
+    boxShadow: boolean,
+    contentSX: object,
+    darkTitle: boolean,
+    divider: boolean,
+    elevation: number,
+    secondary: ReactNode,
+    shadow: string,
+    sx: object,
+    title: string,
+    codeHighlight: boolean,
+    content: boolean,
+    children: ReactNode
+}
+const MainCard = forwardRef(
+    (
+        {
+            border = true,
+            boxShadow,
+            children,
+            content = true,
+            contentSX = {},
+            darkTitle,
+            divider = true,
+            elevation,
+            secondary,
+            shadow,
+            sx = {},
+            title,
+            codeHighlight,
+            ...others
+        }: Props,
+        ref:any
+    ) => {
+        const theme = useTheme();
+        boxShadow = theme.palette.mode === 'dark' ? boxShadow || true : boxShadow;
+
+        return (
+            <Card
+                elevation={elevation || 0}
+                ref={ref}
+                {...others}
+                sx={{
+                    ...sx,
+                    border: border ? '1px solid' : 'none',
+                    borderRadius: 2,
+                    borderColor: theme.palette.mode === 'dark' ? theme.palette.divider : theme.palette.grey.A800,
+                    boxShadow: boxShadow && (!border || theme.palette.mode === 'dark') ? shadow || theme.customShadows.z1 : 'inherit',
+                    ':hover': {
+                        boxShadow: boxShadow ? shadow || theme.customShadows.z1 : 'inherit'
+                    },
+                    '& pre': {
+                        m: 0,
+                        p: '16px !important',
+                        fontFamily: theme.typography.fontFamily,
+                        fontSize: '0.75rem'
+                    }
+                }}
+            >
+                {/* card header and action */}
+                {!darkTitle && title && (
+                    <CardHeader sx={headerSX} titleTypographyProps={{ variant: 'subtitle1' }} title={title} action={secondary} />
+                )}
+                {darkTitle && title && (
+                    <CardHeader sx={headerSX} title={<Typography variant="h3">{title}</Typography>} action={secondary} />
+                )}
+
+                {/* content & header divider */}
+                {title && divider && <Divider />}
+
+                {/* card content */}
+                {content && <CardContent sx={contentSX}>{children}</CardContent>}
+                {!content && children}
+
+                {/* card footer - clipboard & highlighter  */}
+                {codeHighlight && (
+                    <>
+                        <Divider sx={{ borderStyle: 'dashed' }} />
+                        <Highlighter codeHighlight={codeHighlight} main>
+                            {children}
+                        </Highlighter>
+                    </>
+                )}
+            </Card>
+        );
+    }
+);
+
+export default MainCard;
